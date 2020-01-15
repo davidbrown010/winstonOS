2ec6b145c6e216e7706b47322ee08f10b15c0a8d
davidbrown010 <57553756+davidbrown010@users.noreply.github.com>
2020-01-15 15:54:00 -0600

    Update bible.js

--- scripts/bible.js
+++ scripts/bible.js
@@ -5,9 +5,13 @@ refreshBible();
 function refreshBible(){
 	var apiToken = "M5bdALTzNSIPmNuXMBdR8qMPOtA";
 	var time = new Date();
-	var day = (Date.now()/(86400000)).toFixed(0);
-	console.log(Date.now());
-	console.log(day);
+	
+	var now = new Date();
+	var start = new Date(now.getFullYear(), 0, 0);
+	var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+	var oneDay = 1000 * 60 * 60 * 24;
+	var day = Math.floor(diff / oneDay);
+	
 	fetch('https://developers.youversionapi.com/1.0/verse_of_the_day/' + day + '?version_id=206', {
 	    headers: {
 	        'X-YouVersion-Developer-Token': apiToken,
